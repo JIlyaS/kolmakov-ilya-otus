49f1db5aead7cf10bdf8159138cbb31aa60d4038
Колмаков Илья Григорьевич (4057328) <ikolmakov@vtb.ru>
2022-01-29 21:22:47 +0500

    2 add promiseReduce function

---
+++ pro-javascript-2/index.js
@@ -0,0 +1,36 @@
+const fn1 = () => {
+  console.log('fn1');
+  return Promise.resolve(1);
+}
+
+const fn2 = () => new Promise(resolve => {
+  console.log('fn2');
+  setTimeout(() => resolve(2), 1000);
+})
+
+// Добавил для проверки
+const fn3 = () => new Promise(resolve => {
+  console.log('fn3');
+  setTimeout(() => resolve(3), 500);
+})
+
+function promiseReduce(asyncFunctions, reduce, initialValue) { 
+  let memo = initialValue;
+  return new Promise(async (resolve) => {
+    await asyncFunctions.reduce((acc, item) => {
+      return acc.then(item).then((value) => {
+        memo = reduce(memo, value)
+      })
+    }, Promise.resolve())
+    await resolve(memo);
+  });
+}
+
+promiseReduce(
+  [fn1, fn2, fn3], 
+  function (memo, value) {
+    console.log('reduce')
+    return memo * value
+  }, 
+  1
+).then(console.log) 
